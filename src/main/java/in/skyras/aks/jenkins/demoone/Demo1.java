e344654080e3e60dec159306aa62825c2299f3e0
Anil Sharma <anil@aks-saas.kodr.in>
2020-06-13 15:56:03 +0530

    Step 1 Added

---
+++ src/main/java/in/skyras/aks/jenkins/demoone/Demo1.java
@@ -0,0 +1,9 @@
+package in.skyras.aks.jenkins.demoone;
+
+import java.util.Date;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Sample1 Run Successfully at " + new Date());
+    }
+}
